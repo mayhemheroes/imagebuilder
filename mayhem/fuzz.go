5ecc9eab495702175b821dfdd20434d2570f5822
bmwtsn098 <bmwtsn99@gmail.com>
2022-12-20 15:53:26 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzz.go
@@ -0,0 +1,42 @@
+package fuzz
+
+import "strconv"
+import "strings"
+import "github.com/openshift/imagebuilder/dockerfile/parser"
+import "github.com/openshift/imagebuilder/strslice"
+import "github.com/openshift/imagebuilder/signal"
+
+
+func mayhemit(bytes []byte) int {
+
+    var num int
+    if len(bytes) > 2 {
+        num, _ = strconv.Atoi(string(bytes[0]))
+        bytes = bytes[1:]
+
+        switch num {
+    
+        case 0:
+            var test strslice.StrSlice
+            test.UnmarshalJSON(bytes)
+            return 0
+
+        case 1:
+            content := strings.NewReader(string(bytes))
+            parser.Parse(content)
+            return 0
+
+        default:
+            content := string(bytes)
+            signal.CheckSignal(content)
+            return 0
+
+        }
+    }
+    return 0
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
